19bd1eabe7ad4ec07d153efc9189ab303743f12b
MdialloC19 <mdiallo3211@gmail.com>
2023-04-22 02:23:42 +0000

    Console manipuling

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,17 @@
+console.log(5);
+console.log(" Hellow world");
+console.log(20,"Hellow ");
+
+console.error("Alert");
+console.warn("Attention");
+console.table({name:'brad', email: 'musa@gmail.com'});
+console.group("Simple");
+
+console.log(20,"Hellow ");
+
+console.error("Alert");
+console.warn("Attention");
+console.groupEnd();
+
+const styles='padding:10 px; background-color: white; color:green';
+console.log('%cHello World', styles);

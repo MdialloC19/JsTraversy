c5bd24b66f86cec6953888f333abda68ce264abc
MdialloC19 <mdiallo3211@gmail.com>
2023-04-23 15:21:51 +0000

    Data types

---
+++ 01-variables-data-types/04-Data-types/script.js
@@ -0,0 +1,28 @@
+//String 
+const firstName='Sara';
+// Number
+const age=30;
+const temp=98.1;
+//Boolean
+const hasKids=true;
+
+//NUll
+const aptNumber=null;
+
+//Undefinied 
+const score =undefined;
+// Symbol 
+const id=Symbol('id');
+// BigInt
+
+const n=99999999999999993n;
+// refences Types . Any reference types are object
+const numbers=[1,2,3,4];
+//functions sayHello()
+
+function sayHello (){
+    console.log('Hello');
+}
+const output=sayHello;
+
+console.log(output, typeof output);

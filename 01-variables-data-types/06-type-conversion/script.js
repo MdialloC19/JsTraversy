008c5b1dd9c314e6750d9a4460649583b0eb180a
MdialloC19 <mdiallo3211@gmail.com>
2023-04-23 18:26:58 +0000

    Type & Conversion

---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,23 @@
+let amount ='hello';
+//Convert a string to a number
+amount=parseInt(amount);
+//amount+=amount;
+//amount=Number(amount);
+
+
+//Convert number to string
+//creation of temporary wrapper for number
+
+//amount=amount.toString();
+
+//Convert a string in decimal
+//amount=String(amount);
+//amount=parseFloat(amount);
+
+//Convert number to boolean
+//amount =Boolean(amount);
+console.log(Math.sqrt(-1));
+console.log(1+NaN);
+console.log(undefined+undefined);
+console.log('foo'/3);
+console.log(amount, typeof amount);

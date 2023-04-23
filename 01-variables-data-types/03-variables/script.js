c966996ea9da2eec27a9056f8978a2f1eb6ea2d8
MdialloC19 <mdiallo3211@gmail.com>
2023-04-23 14:16:52 +0000

    Variables

---
+++ 01-variables-data-types/03-variables/script.js
@@ -0,0 +1,43 @@
+// Ways to declare a variable
+//var, let ,const
+let firstName='John';
+let lastName='Doe';
+
+console.log(firstName,lastName);
+let age=30;
+
+console.log(age);
+//Naming Conventions
+//-Only, letters,numbers,underscores and dollar signs
+//-Can't start with a number1
+
+//Re-assigning variables
+
+age=31;
+console.log(age);
+let score;
+score =1;
+console.log(score);
+if(true){
+    score=score+1;
+}
+console.log(score);
+const x=100;
+
+const arr= [1,2,4,3,4];
+
+arr.push(5);
+console.log(arr);
+
+const person={
+    name: 'Brad',
+
+};
+person.name='John';
+person.email='Musa@gmail.com';
+
+console.log(person);
+//Declare  multiple values at once
+let a, b,c;
+const d=10, f=10, e =50;
+console.log(d);

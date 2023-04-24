1af9657cc9590ffd9ad13710e219a7d4bd63ab7d
MdialloC19 <mdiallo3211@gmail.com>
2023-04-24 03:44:51 +0000

    Type

--- 01-variables-data-types/07-operators/script.js
+++ 01-variables-data-types/07-operators/script.js
@@ -6,4 +6,32 @@ x=5-5;
 x=5*5;
 
 
+
+//Concatenation
+
+x='hello'+' '+ 'World';
+//Exponent
+x=2**3
+
+//Increment
+x=1;
+x++;
+//Decrement 
+x--;
+//Assignment Operators
+
+x=10;
+x+=5;
+
+//Comparaison
+x=2==4;
+
+x=2==='2';// triple equal compare the value and the types of object
+
+x=2!=2;
+
+x=2!==2;
+
+
+
 console.log(x);

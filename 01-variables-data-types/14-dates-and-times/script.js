f5fb9fb1b7ada9a1cb658cb36f84d10ebeb8120c
MdialloC19 <mdiallo3211@gmail.com>
2023-04-25 17:38:55 +0000

    Date and times

---
+++ 01-variables-data-types/14-dates-and-times/script.js
@@ -0,0 +1,19 @@
+let d;
+d=new Date();
+
+d=d.toString();
+
+d =new Date(2021,6,10,30,0);
+
+d=new Date('2021-07-10T12:30:00');
+
+d=new Date('07/10/2021 12:30:00');
+
+d=new Date('2022-12-28');
+
+d=new Date();
+d=d.getTime();
+d=d.valueOf();
+d=new Date(1682444240553);
+d=Math.floor(Date.now()/1000);
+console.log(d);

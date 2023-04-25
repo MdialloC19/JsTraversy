c50a4e8a0fe8b0044bba5b7b87579c47172ef729
MdialloC19 <mdiallo3211@gmail.com>
2023-04-25 02:31:52 +0000

    String methods

---
+++ 01-variables-data-types/09-strings/script.js
@@ -0,0 +1,28 @@
+let x;
+const name ='John';
+const age=31;
+x='hello my name is '+name+' and i am '+age+ ' years old';
+// Template literals
+x = `Hello, my name is ${name} and I am ${age} years`;
+
+// String Properties and Methods
+const s=new String('Hello World');
+x= typeof s;
+x=s.length;
+//Access value by keys
+
+x=s[6];
+x=s.__proto__;
+x=s.toLowerCase();
+x=s.charAt(0);
+x=s.indexOf('e');
+x=s.substring(1,4);
+x=s.slice(-11,-5);
+x='                                Hello World';
+x=s.trim();
+x=s.replace('World', 'john');
+x=s.includes('ello');
+x=s.valueOf();
+x=s.split('');
+
+console.log(x);

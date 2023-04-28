d3361dbb19a91b73111ad6ea57d8bf2b24a964ff
MdialloC19 <mdiallo3211@gmail.com>
2023-04-28 12:36:33 +0000

    Array introduction

---
+++ 02-arrays-and-object/01-array-basics/script.js
@@ -0,0 +1,16 @@
+
+let x;
+//Arrays literal
+const numbers=[12,13,33,29,39];
+
+//Array Constructor
+
+const fruits=new Array('apple', 'orange', 'tomato');
+x=numbers[0];
+x=`My favorite fruits is ${fruits[2]}`;
+x=numbers.length;
+fruits[2]='pear';
+//fruits.length=2;
+fruits[3]='strawberry';
+
+console.log(fruits);

edc3b4953234afcae5f6567046616c5ce7960fe5
MdialloC19 <mdiallo3211@gmail.com>
2023-05-01 21:21:39 +0000

    Challenge array

---
+++ 02-arrays-and-object/04-array-chalenge/scripts.js
@@ -0,0 +1,13 @@
+// Challenge 1;
+
+const arr=[1,2,3,4,5];
+ arr.push(6);
+ x=arr.reverse();
+
+//Chalenge 2
+const arr1= [1,2,3,4,5];
+const arr2=[5,6,7,8,9,10];
+
+arr3=[...arr1, ...arr2.slice(1)]
+
+console.log(arr3);

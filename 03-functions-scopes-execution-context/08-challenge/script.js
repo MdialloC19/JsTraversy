1b86b97e043a704ef4563b34b87c6c6b38b7dbd3
MdialloC19 <mdiallo3211@gmail.com>
2023-05-08 23:54:18 +0000

    Function  challenge

---
+++ 03-functions-scopes-execution-context/08-challenge/script.js
@@ -0,0 +1,29 @@
+// Challenge 1
+const getCelsius= (temp)=> (temp -32)*5/9;
+
+console.log(`The temperature is ${getCelsius(44)} \x80 C`);
+    
+//Challenge 2
+
+//first method
+// const minMax=(arrow )=> {
+//     arrow.sort((a,b)=>a-b);
+//     obminmax={
+//         'min': arrow[0],
+//         'max': arrow[arrow.length-1]
+//     }
+//     return obminmax;
+// }
+
+// Second method
+const minMax=(arrow)=>{
+    const min=Math.min(...arrow);
+    const max=Math.max(...arrow);
+       
+    return {
+        min,
+        max
+    } 
+}
+
+console.log(minMax([2,4,3,10,24,0,1,-5,11]));

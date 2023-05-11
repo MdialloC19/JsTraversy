3fff72caf17c9448ffbd2336437abfe96e48b105
MdialloC19 <mdiallo3211@gmail.com>
2023-05-11 16:32:49 +0000

    Logical assigment

--- 04-logic-control-flow/05-truthy-falsy/script.js
+++ 04-logic-control-flow/05-truthy-falsy/script.js
@@ -10,4 +10,30 @@ if(x){
     console.log('This is falsy');
 }
 
+// falsy assigment 
+
+let a=false 
+
+// if(!a){
+//     a=10
+// }
+
+a=a ||10;
+
+//&& =assign the right side value only if left os truthy
+let b=0;
+b=b && 20 // b&&=20
+console.log(b);
+
+// ??= assigns the right side value only if the left is null or undefined 
+
+let c=null
+
+if(c===null || c===undefined){
+    c=20;
+}
+
+c=c??20;
+c ??=20;
+console.log(c);
 console.log(Boolean(x));

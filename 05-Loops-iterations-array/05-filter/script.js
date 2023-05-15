4892c56dd29f1d4b0f9b0c456d785f3bd677caa8
MdialloC19 <mdiallo3211@gmail.com>
2023-05-15 01:31:45 +0000

    Filter

--- 05-Loops-iterations-array/05-filter/script.js
+++ 05-Loops-iterations-array/05-filter/script.js
@@ -5,6 +5,34 @@ const numbers =[1,2,3,4,5,6,7,8,9,10];
 // });
 
 //short version
-const evenNumbers=numbers.filter((numbers) =>numbers %2===0);
+ //const evenNumbers=numbers.filter((number) =>number %2===0);
 
-console.log(evenNumbers);
+// With foreach
+
+//  let evenNumbers=[];
+// numbers.forEach( number=> {
+//     if(number%2===0){
+//         evenNumbers.push(number);
+//     }
+// });
+
+const companies =[
+    {name: 'Compagny One', category: 'Finance', start:1989, end: 2004},
+    {name: 'Compagny 2', category: 'BOok', start:1981, end: 2004},
+    {name: 'Compagny 3', category: 'consulting', start:1981, end: 2008},
+    {name: 'Compagny 4 ', category: 'Retail', start:1981, end: 2009},
+    {name: 'Compagny 5', category: 'Finance', start:1921, end: 2000},
+    {name: 'Compagny 6', category: 'Retail', start:1981, end: 2001},
+    {name: 'Compagny 7', category: 'Retail', start:1920, end: 2002}
+];
+
+// companies.forEach((companie)=>{
+    //         if (companie['category']=='Retail')
+    //             Retail.push(companie['name']);
+    
+    
+    // });
+    
+   const Retail= companies.filter((
+    companie)=> companie.category==='Retail');
+console.log(Retail);

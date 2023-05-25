16819d3ed5e10a7e6d7e2310b1e8e56820be34d3
MdialloC19 <mdiallo3211@gmail.com>
2023-05-25 22:15:12 +0000

    Intro DOM

---
+++ 06-dom/script.js
@@ -0,0 +1,16 @@
+// console.dir(window.document);
+
+// console.log(document.body.innerHTML);
+
+console.log(document.links[0]);
+
+// document.body.innerHTML='<h1>Hello World</h1>'
+
+// document.write("Hello wordld");
+
+//console.log(document.getElementById('main'));
+
+const main=document.getElementById('main');
+//  main.innerHTML='<h1>Hello from main</h1>'
+
+document.querySelector("#main p").innerText='Hello world';

d2aebff6c8b47782d2f35aaed8117bf82de440b5
MdialloC19 <mdiallo3211@gmail.com>
2023-05-26 17:06:06 +0000

    Query select All

---
+++ 06-dom/04-dom-select-multiple/shopping-list/script.js
@@ -0,0 +1,36 @@
+//querySelectorAll()
+
+const listItems=document.querySelector('.item');
+
+console.log(listItems);
+
+// listItems[1].style.color='red';
+
+// listItems.forEach((elt, index)=> {
+//     elt.style.color='red';
+
+//     if(index===1){
+//         elt.remove();
+//     }
+//     if(index===0){
+//         elt.innerHTML=`Orange
+//         <button class="remove-item btn-link text-red">
+//           <i class="fa-solid fa-xmark"></i>
+//         </button>`;
+//     }
+// });
+
+const listItems2=document.getElementsByClassName('item');
+
+console.log(listItems2[2].innerText);
+listItems2Array=Array.from(listItems2);
+
+listItems2Array.forEach(element => {
+    console.log(element.innerText);
+    
+});
+
+const listItems3=document.getElementsByTagName('li');
+
+console.log(listItems3);
+

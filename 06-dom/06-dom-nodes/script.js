31a17c555b08edaa4c2795711d920467d8169089
MdialloC19 <mdiallo3211@gmail.com>
2023-05-29 20:55:16 +0000

    nodes element

---
+++ 06-dom/06-dom-nodes/script.js
@@ -0,0 +1,38 @@
+let output;
+
+const parent=document.querySelector('.parent');
+
+output=parent.childNodes[0].nodeName;
+output=parent.childNodes[3].textContent;
+output=parent.childNodes[3].outerHTML;
+
+output=parent.childNodes[3].innerText='Child One';
+
+output=parent.childNodes[5].style.color='red';
+
+output=parent.firstChild;
+
+output=parent.lastChild;
+
+parent.lastChild.textContent="Hello you";
+
+// Parent node
+
+const child=document.querySelector('.child');
+
+
+output=child.parentNode;
+output=child.parentElement.style.backgroundColor="#ccc";
+
+child.parentElement.style.padding="10px";
+
+// Sibblings
+
+const secondItem=document.querySelector('.child:nth-child(2)');
+
+
+output=secondItem.nextSibling;
+// output=secondItem.previousSibling;
+
+
+console.log(output);

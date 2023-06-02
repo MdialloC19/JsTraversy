328242ff32cd8f2e4351320581f133b597d67836
MdialloC19 <mdiallo3211@gmail.com>
2023-06-02 01:40:23 +0000

    create element in the dom

---
+++ 06-dom/07-dom-create-element/shopping-list/script.js
@@ -0,0 +1,29 @@
+// const elt= document.createElement('li');
+
+// elt.setAttribute('class','item');
+// const text=document.createTextNode(`Banana <button class="remove-item btn-link text-red">
+// <i class="fa-solid fa-xmark"></i></button>`);
+
+// elt.appendChild(text);
+
+// const list=document.querySelector('ul');
+
+// list.appendChild(elt);
+
+// div.className='my-element';
+// div.id='my-element';
+// div.setAttribute('title', 'My element');
+
+// // div.innerText='Hello World';
+
+// const text =document.createTextNode('Hello World');
+
+// div.appendChild(text);
+
+// document.body.appendChild(div);
+
+// //
+// const elt=document.querySelector('.items');
+// elt.children[1].appendChild(div);
+
+console.log(elt);

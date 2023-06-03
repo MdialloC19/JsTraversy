bdfc4affefd86717acfc75e5f05d2bf81c47999d
MdialloC19 <mdiallo3211@gmail.com>
2023-06-03 14:33:30 +0000

    Challenge dom

--- 06-dom/10-dom_challlenge/shopping-list/script.js
+++ 06-dom/10-dom_challlenge/shopping-list/script.js
@@ -2,7 +2,26 @@
 
 // function inseertAfter
 
-function insertAfter(item,elt){
+function insertAfter(elt,ref){
 
-    elt.insertAdja
+    ref.insertAdjacentHTML('afterEnd',elt);
+
+    
 }
+
+const ul =document.querySelector('ul');
+
+const li=document.createElement('li');
+// li.textContent='Bannana';
+li.innerHTML=`Banana <button class="remove-item btn-link text-red">
+<i class="fa-solid fa-xmark"></i></button>`; 
+
+const thirdItem=document.querySelector('li:nth-child(3)');
+console.log(li);
+
+
+function insertAfter2(newElt, existEl) {
+    (existEl.parentNode).insertBefore(newElt, existEl.nextSibling);
+  }
+
+insertAfter(li.innerHTMLinnerHTML,thirdItem);

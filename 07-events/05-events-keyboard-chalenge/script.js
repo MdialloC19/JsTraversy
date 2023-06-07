989dd81a91afe3293e14be6d05abefaa21e122b1
MdialloC19 <mdiallo3211@gmail.com>
2023-06-07 01:35:02 +0000

    Key challenge

---
+++ 07-events/05-events-keyboard-chalenge/script.js
@@ -0,0 +1,79 @@
+// const itemInput=document.getElementById('item-input');
+
+// const keyParent=document.querySelector('.key');
+// console.log(keyParent.childNodes[0]);
+// // const theKey=document.querySelector('key');
+// const onKeyDown=(e)=>{
+//     console.log(e.key)
+//     if(e.key){
+//         keyParent.childNodes[0].innerText=e.key;
+//     }
+    
+// };
+
+//Method 1
+
+// window.addEventListener('keydown',(e)=>{
+    
+//     const div=document.getElementById('insert');
+    
+//     insert.innerHTML=`
+//         <div class="key">
+//         ${e.key===' '?'Space':e.key}
+//         <small>e.key</small>
+//         </div>
+
+//          <div class="key">
+//             ${e.keyCode}
+//         <small>e.keyCode</small>
+//         </div>
+//         <div class="key">
+//             ${e.code}
+//         <small>e.code</small>
+//         </div>
+//     `;
+// });
+
+
+function showKeyCodes(e){
+    const insert=document.getElementById('insert');
+
+    insert.innerHTML='';
+
+    const keyCode={
+        'e.key':e.key===' '?'Space':e.key,
+        'e.keyCode':e.keyCode,
+        'e.code': e.code,
+    };
+
+    for(let key in keyCode){
+        const div =document.createElement('div');
+        div.className='key';
+
+        const small=document.createElement('small');
+
+        const keyText=document.createTextNode(key);
+        const valueText=document.createTextNode(keyCode[key]);
+
+        small.appendChild(keyText);
+        div.appendChild(valueText);
+        div.appendChild(small);
+        insert.appendChild(div);
+    }
+    //     insert.innerHTML=`<div class="key">
+    //             ${keyCode['e.key']}
+    //             <small>e.key</small>
+    //             </div>
+        
+    //              <div class="key">
+    //                 ${keyCode['e.keyCode']}
+    //             <small>e.keyCode</small>
+    //             </div>
+    //             <div class="key">
+    //                 ${keyCode['e.code']}
+    //             <small>e.code</small>
+    //             </div>`
+
+    // console.log(keyCode);
+}
+window.addEventListener('keydown',showKeyCodes);

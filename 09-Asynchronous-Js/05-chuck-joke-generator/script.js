521b7eb12792a59bf6839891c57781ea2827f499
MdialloC19 <mdiallo3211@gmail.com>
2023-11-10 17:36:29 +0000

    XMLHttpRequest

---
+++ 09-Asynchronous-Js/05-chuck-joke-generator/script.js
@@ -0,0 +1,36 @@
+ const jokeEl=document.querySelector('#joke');
+const jokeBtn=document.querySelector('#joke-btn');
+// readyState has 5 possible values
+// - 0: request not initialized
+// - 1: server connection established
+// - 2: request received
+// - 3: processing request
+
+// - 4: request finished and response is ready
+
+
+
+const generateJoke = ()=>{
+    const xhr=new XMLHttpRequest();
+
+    xhr.open('GET','https://api.chucknorris.io/jokes/random');
+    xhr.onreadystatechange= function(){
+    
+    if(this.readyState===4){
+
+        if ( this.status ===200){
+            // console.log(JSON.parse(this.responseText));
+            const data =JSON.parse(this.responseText);
+           
+              
+            jokeEl.innerHTML=data.value;      
+        }else {
+            jokeEl.innerHTML='Something Went Wrong (Not  Funny)';
+        }
+    }
+};
+xhr.send();
+}
+
+jokeBtn.addEventListener('click', generateJoke);
+document.addEventListener('DOMContentLoaded', generateJoke);

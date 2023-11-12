a91a8845ceb38d4d6bc45f95ed8d5470fe81a9cb
MdialloC19 <mdiallo3211@gmail.com>
2023-11-12 19:39:55 +0000

    Promise & callback hell and Promise.all

---
+++ 09-Asynchronous-Js/09-Promise-callback-hell/script.js
@@ -0,0 +1,44 @@
+function getData(endPoint){
+
+    return new Promise((resolve,reject)=>{
+        const xhr=new XMLHttpRequest();
+        xhr.open('GET', endPoint);
+    
+        xhr.onreadystatechange=function(){
+            if(this.readyState==4){
+                if( this.status===200){
+                    resolve(JSON.parse(this.responseText));
+                }else{
+                    reject('Something went wrong');
+                }
+            }
+        }
+        setTimeout(()=>{
+            xhr.send();
+        }, Math.floor(Math.random()*3000+ 1000));
+    });
+};
+
+getData('./movies.json')
+    .then((movies)=>{
+    console.log(movies);
+    return getData("./actors.json");
+    })
+    .then((actors)=>{
+        console.log(actors);
+        return getData("./directors.json ");
+    })
+    .then((directors)=>{
+        console.log(directors);
+    })
+    .catch((error)=> console.log(error));
+
+// getData('./movies.json', (data)=>{
+//     console.log(data);
+//     getData('./actors.json', (data)=>{
+//         console.log(data);
+//         getData('./directors.json', (data)=>{
+//             console.log(data);
+//         })
+//     });
+// });
